0910390b8daeeeec226ed51a577baf14f6217885
Brett Gullan <brettgullan@mac.com>
2019-10-21 15:35:43 +1100

    feat: Extract storybook defaults

---
+++ src/theme/storybook.js
@@ -0,0 +1,31 @@
+export const fontWeights = {
+  Thin: 'thin',
+  Light: 'light',
+  Regular: 'regular',
+  SemiBold: 'semiBold',
+  Bold: 'bold',
+  ExtraBold: 'extraBold',
+  Black: 'black',
+}
+
+export const fontFamily = {
+  Text: 'text',
+  Title: 'title',
+}
+
+export const textTransforms = {
+  None: 'none',
+  Uppercase: 'uppercase',
+  Lowercase: 'lowercase',
+  Capitalize: 'capitalize',
+}
+
+export const colors = {
+  White: '#FFF',
+  Drive: '#00C8C0',
+  Grey: '#4A4A4A',
+  Black: '#141414',
+  'Real Black': '#000',
+}
+
+export const defaultColor = colors.Black
