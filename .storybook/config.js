dca56449fcfd6365a47f2b418b7213b59a783cdb
Brett Gullan <brettgullan@mac.com>
2019-10-21 12:52:50 +1100

    feat: Add Storybook Addons and configure

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,18 +1,39 @@
 import React from 'react'
-import { configure } from '@storybook/react'
-import { addDecorator } from '@storybook/react'
-import { withThemesProvider } from 'storybook-addon-emotion-theme'
+import { addDecorator, addParameters, configure } from '@storybook/react'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import { ThemeProvider } from 'emotion-theming'
+
+// ----------------------------------------------------------------------------
 
 import theme from '../src/theme'
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.js$/), module)
-
-// const themes = [theme]
-//addDecorator(withThemesProvider(themes))
+// ----------------------------------------------------------------------------
 
 const ThemeDecorator = (storyFn) => (
   <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
 )
 addDecorator(ThemeDecorator)
+
+// ----------------------------------------------------------------------------
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+    defaultViewport: 'someDefault',
+  },
+})
+
+addParameters({
+  backgrounds: [
+    { name: 'white', value: '#FFF' },
+    { name: 'base', value: '#F5F5F5', default: true },
+    { name: 'drive', value: '#00C8C0' },
+    { name: 'grey', value: '#666' },
+    { name: 'black', value: '#000' },
+  ],
+})
+
+// ----------------------------------------------------------------------------
+
+// automatically import all files ending in *.stories.js
+configure(require.context('../src', true, /\.stories\.(js|mdx)$/), module)
