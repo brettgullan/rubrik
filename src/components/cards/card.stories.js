763f419a9b080dc535f5d8e74b332defb978f2a9
Brett Gullan <brettgullan@mac.com>
2019-10-29 11:11:34 +1100

    wip: Add Card stories exploring lazy loading effects options
    LazySizes BlurUp plugin seems to work:
    - Needs more “real world” testing
    - Needs better abstraction/componentization
    
    Adding blurUp styles to a wrapper component (i.e. Scrim) while configuring options on the Image component doesn’t work/scale.

--- src/components/cards/card.stories.js
+++ src/components/cards/card.stories.js
@@ -1,17 +1,19 @@
 import React, { cloneElement } from 'react'
 import { Box, Flex, Link, Text } from 'rebass'
+import { padding, position, size } from 'polished'
 
 // ----------------------------------------------------------------------------
 
 import Cloudinary from '../../providers/Cloudinary'
+import LazyLoadProvider from '../../providers/LazySizes'
 
 // ----------------------------------------------------------------------------
 
 import Card from './card'
 import Price from './price'
-import { FauxBlockLink } from '../structure'
 import { Standard as Image } from './images'
-import { Tile, TileLayer, Scrim } from '../structure'
+import { FauxBlockLink, Tile, TileLayer, Scrim } from '../structure'
+import { LazyLoad } from '../media'
 
 // ----------------------------------------------------------------------------
 
@@ -28,46 +30,18 @@ const withCloudinary = (storyFn) => (
   </Cloudinary>
 )
 
+const withLazyLoad = (storyFn) => (
+  <LazyLoadProvider>{storyFn()}</LazyLoadProvider>
+)
+
 // ----------------------------------------------------------------------------
 
 export default {
   title: 'Design System|Elements/Cards/Card',
-  decorators: [withCloudinary],
+  decorators: [withCloudinary, withLazyLoad],
 }
 
 // ----------------------------------------------------------------------------
-
-const buildIntrinsicRatioStyles = (ratio) => ({
-  position: 'relative',
-  '&:before': {
-    /* This will setup the aspect ratio of our screen */
-    content: '""',
-    display: 'block',
-    /* content-box makes sure padding adds to declared height */
-    boxSizing: 'content-box',
-    width: '100%',
-    height: 0,
-    /* Vertical padding is based on parent element's width */
-    /* So we want 9/16, converted to % as our vertical padding */
-    paddingBottom: `${(1 / ratio) * 100}%`,
-  },
-})
-
-const scrimStyle = () => ({
-  '&:after': {
-    /* This will setup the aspect ratio of our screen */
-    content: '""',
-    display: 'block',
-    position: 'absolute',
-    /* content-box makes sure padding adds to declared height */
-    boxSizing: 'content-box',
-    width: '100%',
-    height: '100%',
-    /* Vertical padding is based on parent element's width */
-    /* So we want 9/16, converted to % as our vertical padding */
-    backgroundImage: 'linear-gradient(to top, #000, rgba(0,0,0,.4) 40%)',
-  },
-})
 
 export const Default = () => {
   const image = {
@@ -77,7 +51,7 @@ export const Default = () => {
   return (
     <Card>
       <Tile>
-        <Image image={image} sx={buildIntrinsicRatioStyles(16 / 9)} />
+        <Image image={image} />
         <TileLayer p={4} justifyContent="flex-end">
           <Text variant="trafalgar" fontWeight="regular" color="white.0">
             2020 Mercedes-AMG GT Review
@@ -94,6 +68,8 @@ export const Default = () => {
     </Card>
   )
 }
+
+// ----------------------------------------------------------------------------
 
 export const BlockLink = () => {
   const image = {
@@ -126,6 +102,8 @@ export const BlockLink = () => {
   )
 }
 
+// ----------------------------------------------------------------------------
+
 export const WithScrim = () => {
   const image = {
     id: 'xebemoumy9losdljfk1x',
@@ -136,6 +114,178 @@ export const WithScrim = () => {
       <Tile>
         <Scrim>
           <Image image={image} />
+        </Scrim>
+        <TileLayer p={4} justifyContent="flex-end">
+          <Text variant="trafalgar" fontWeight="regular" color="white.0">
+            2020 Volkswagen Golf GTI Review
+          </Text>
+        </TileLayer>
+      </Tile>
+      <Box p={4}>
+        <Text variant="pica" mb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Text>
+        <Price rangeStart="$30,490" rangeEnd="$42,990" type="MSRP" />
+      </Box>
+    </Card>
+  )
+}
+
+// ----------------------------------------------------------------------------
+
+const withIntrinsicRatio = (ratio) => ({
+  position: 'relative',
+  overflow: 'hidden',
+  '&:before': {
+    /* This will setup the aspect ratio of our screen */
+    content: '""',
+    display: 'block',
+    /* content-box makes sure padding adds to declared height */
+    boxSizing: 'content-box',
+    width: '100%',
+    height: 0,
+    /* Vertical padding is based on parent element's width */
+    /* So we want 9/16, converted to % as our vertical padding */
+    paddingBottom: `${(1 / ratio) * 100}%`,
+  },
+})
+
+const withFullBleed = () => ({
+  display: 'block',
+  ...position('absolute', 0),
+  ...size('100%'),
+  ...padding(0),
+})
+
+const blurUp = {
+  '&.lazyload': {
+    '-webkit-filter': 'blur(8px)',
+    filter: 'blur(8px)',
+    transition: 'filter 250ms, -webkit-filter 250ms',
+  },
+  '&.lazyloaded': {
+    '-webkit-filter': 'blur(0)',
+    filter: 'blur(0)',
+  },
+}
+
+const fadeIn = {
+  '&.lazyload': {
+    opacity: 0,
+    transition: 'opacity 400ms',
+  },
+  '&.lazyloaded': {
+    opacity: 1,
+  },
+}
+
+// ----------------------------------------------------------------------------
+
+export const LazyLoadingFadeIn = () => {
+  const image = {
+    id: 'xebemoumy9losdljfk1x',
+    type: 'private',
+  }
+  return (
+    <Card>
+      <Tile>
+        <Scrim sx={withIntrinsicRatio(16 / 9)} bg="gray.0">
+          <LazyLoad>
+            <Image image={image} sx={{ ...withFullBleed(), ...fadeIn }} />
+          </LazyLoad>
+        </Scrim>
+        <TileLayer p={4} justifyContent="flex-end">
+          <Text variant="trafalgar" fontWeight="regular" color="white.0">
+            2020 Volkswagen Golf GTI Review
+          </Text>
+        </TileLayer>
+      </Tile>
+      <Box p={4}>
+        <Text variant="pica" mb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Text>
+        <Price rangeStart="$30,490" rangeEnd="$42,990" type="MSRP" />
+      </Box>
+    </Card>
+  )
+}
+
+// ----------------------------------------------------------------------------
+
+export const LazyLoadingBlurUp = () => {
+  const image = {
+    id: 'xebemoumy9losdljfk1x',
+    type: 'private',
+  }
+  return (
+    <Card>
+      <Tile>
+        <Scrim sx={withIntrinsicRatio(16 / 9)} bg="gray.0">
+          <LazyLoad>
+            <Image image={image} sx={{ ...withFullBleed(), ...blurUp }} />
+          </LazyLoad>
+        </Scrim>
+        <TileLayer p={4} justifyContent="flex-end">
+          <Text variant="trafalgar" fontWeight="regular" color="white.0">
+            2020 Volkswagen Golf GTI Review
+          </Text>
+        </TileLayer>
+      </Tile>
+      <Box p={4}>
+        <Text variant="pica" mb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Text>
+        <Price rangeStart="$30,490" rangeEnd="$42,990" type="MSRP" />
+      </Box>
+    </Card>
+  )
+}
+
+// ----------------------------------------------------------------------------
+
+export const LazyLoadingBlurUpPlugin = () => {
+  const image = {
+    id: 'xebemoumy9losdljfk1x',
+    type: 'private',
+  }
+
+  const options = {
+    srcset: Image.defaultProps.options.srcset,
+    'data-lowsrc': Image.defaultProps.options.placeholder,
+  }
+
+  const blurUpPluginStyles = {
+    '.ls-blur-up-is-loading, .lazyload:not([src])': {
+      visibility: 'hidden',
+    },
+    '.ls-img, .ls-blur-up-img': {
+      ...withFullBleed(),
+      objectFit: 'cover',
+    },
+    '.ls-blur-up-img': {
+      filter: 'blur(10px)',
+      opacity: 1,
+      transition: 'opacity 1000ms, filter 1500ms',
+    },
+    '.ls-blur-up-img.ls-inview.ls-original-loaded': {
+      opacity: 0,
+      filter: 'blur(5px)',
+    },
+  }
+
+  return (
+    <Card>
+      <Tile>
+        <Scrim
+          sx={{ ...withIntrinsicRatio(16 / 9), ...blurUpPluginStyles }}
+          bg="gray.0"
+        >
+          <LazyLoad>
+            <Image className="ls-img" image={image} options={options} />
+          </LazyLoad>
         </Scrim>
         <TileLayer p={4} justifyContent="flex-end">
           <Text variant="trafalgar" fontWeight="regular" color="white.0">
