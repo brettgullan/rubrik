a49fc574420b6540f697e2dd5162b04936b5960e
Brett Gullan <brettgullan@mac.com>
2019-10-29 11:08:41 +1100

    feat: Specify Card Image options as default prop
    Allows external access and override.

--- src/components/cards/images.js
+++ src/components/cards/images.js
@@ -6,22 +6,30 @@ import { CloudinaryImage } from '../media'
 
 // ----------------------------------------------------------------------------
 
-export const Standard = ({ image, sx, ...rest }) => {
-  const imageOptions = {
+export const Standard = ({ image, options, sx, ...rest }) => {
+  return (
+    <CloudinaryImage
+      image={image}
+      options={options}
+      sx={{ display: 'block', width: '100%', ...sx }}
+      {...rest}
+    />
+  )
+}
+
+Standard.defaultProps = {
+  options: {
     srcset: {
       widths: [375, 480, 640, 728],
       aspect_ratio: 16 / 9,
       crop: 'fill',
       quality: 80,
     },
-  }
-
-  return (
-    <CloudinaryImage
-      sx={{ display: 'block', width: '100%', bg: 'gray.0', ...sx }}
-      image={image}
-      options={imageOptions}
-      {...rest}
-    />
-  )
+    placeholder: {
+      width: 60,
+      aspect_ratio: 16 / 9,
+      crop: 'fill',
+      quality: 30,
+    },
+  },
 }
