f3e085776b1507311bcb1595e786f4bf09b96145
Brett Gullan <brettgullan@mac.com>
2019-10-31 07:38:39 +1100

    improve: Remove state-based color props
    This was over-egging it. If clients/consumers want to customize the input field, they can pass in an appropriate sx prop, using the CSS classnames for each state, as appropriate.

--- src/components/forms/input.js
+++ src/components/forms/input.js
@@ -2,49 +2,16 @@ import React from 'react'
 import cx from 'classnames'
 
 import { Input as InputField } from '@rebass/forms'
-import { useTheme } from 'emotion-theming'
-import { get } from '@styled-system/css'
-import { tint } from 'polished'
 
 // ----------------------------------------------------------------------------
 
-const Input = ({
-  size,
-  valid,
-  error,
-  color,
-  validColor,
-  errorColor,
-  className,
-  sx,
-  ...rest
-}) => {
-  const theme = useTheme()
-
-  const state = error ? 'error' : valid ? 'valid' : 'default'
+const Input = ({ size, valid, error, className, ...rest }) => {
+  const states = { error: error, valid: valid }
 
   return (
     <InputField
       variant="input"
-      className={cx(className, size, { [state]: state !== 'default' })}
-      sx={{
-        ...(color && {
-          color,
-        }),
-        ...(validColor && {
-          '&.valid': {
-            color: validColor,
-            bg: tint(0.95, get(theme, `colors.${validColor}`, validColor)),
-          },
-        }),
-        ...(errorColor && {
-          '&.error': {
-            color: errorColor,
-            bg: tint(0.95, get(theme, `colors.${errorColor}`, errorColor)),
-          },
-        }),
-        ...sx,
-      }}
+      className={cx(className, size, states)}
       {...rest}
     />
   )
