d020b63335fb2cc0126138a7d8cc72c2bbecf081
Brett Gullan <brettgullan@mac.com>
2019-11-01 06:50:49 +1100

    feat: Implement Submit button

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,2 +1,3 @@
-export { default as Input } from './input'
 export { default as ErrorMessage } from './error'
+export { default as Input } from './input'
+export { default as Submit } from './submit'
