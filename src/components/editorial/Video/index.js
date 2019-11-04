1f0802bf94e7580d627fd9fb3691c22d60116df1
Brett Gullan <brettgullan@mac.com>
2019-11-04 15:01:36 +1100

    feat: Initial Video embed component

---
+++ src/components/editorial/Video/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+import styled from '@emotion/styled'
+import { position, size } from 'polished'
+
+import { Flex } from 'rebass'
+
+// ----------------------------------------------------------------------------
+
+const ResponsivePlayer = styled(ReactPlayer)`
+  ${position('absolute', 0)}
+  ${size('100%')}
+`
+
+// ----------------------------------------------------------------------------
+
+export default ({ url, width, height, config, sx, ...rest }) => {
+  const ratio = `${(height / width) * 100}%`
+
+  return (
+    <Flex
+      sx={{
+        position: 'relative',
+        width: '100%',
+        pt: ratio,
+        ...sx,
+      }}
+      {...rest}
+    >
+      <ResponsivePlayer url={url} config={config} width="100%" height="100%" />
+    </Flex>
+  )
+}
