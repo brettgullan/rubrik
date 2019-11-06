6a3b3146eeb3473b765293de4c8a59ef6f1535ac
Brett Gullan <brettgullan@mac.com>
2019-11-07 08:18:34 +1100

    feat: Use useVariant hook

--- src/components/content/PullQuote/index.js
+++ src/components/content/PullQuote/index.js
@@ -1,22 +1,15 @@
 import React from 'react'
 
-import { useTheme } from 'emotion-theming'
-import { get } from '@styled-system/css'
+// ----------------------------------------------------------------------------
 
 import { Flex } from 'rebass'
+import { useVariant } from '../../../hooks'
 import { Stack } from '../../structure'
 
 // ----------------------------------------------------------------------------
 
-const useVariant = (path, dflt = null) => {
-  const theme = useTheme()
-  return get(theme, path, dflt)
-}
-
-// ----------------------------------------------------------------------------
-
 const PullQuote = ({ quote, attribution, sx, ...rest }) => {
-  const styles = useVariant('content.pullquote', {})
+  const styles = useVariant('content.pullquote')
   const { block: sxBlock, quote: sxQuote, attribution: sxAttribution } = sx
   return (
     <Stack
