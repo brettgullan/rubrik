dc5307157385db5a7104f8c3e7eb042f515ba590
Brett Gullan <brettgullan@mac.com>
2019-11-07 16:38:10 +1100

    wip: Initial ImageBlock component

--- src/components/content/ImageBlock/index.js
+++ src/components/content/ImageBlock/index.js
@@ -5,16 +5,31 @@ import React from 'react'
 import { Flex } from 'rebass'
 import { useImageConfig, useVariant } from '../../../hooks'
 import { Stack } from '../../structure'
-import { Image } from '../../media'
+import { CloudinaryImage } from '../../media'
 
 // ----------------------------------------------------------------------------
 
 const ImageBlock = ({ image, caption, sx, ...rest }) => {
+  const imageOptions = useImageConfig('content.default')
   const styles = useVariant('content.imageblock')
-  const { block: sxBlock, caption: sxCaption } = sx
+  const { block: sxBlock, image: sxImage, caption: sxCaption } = sx
+
+  const imageComponent = (
+    <CloudinaryImage
+      image={image}
+      options={imageOptions}
+      sx={{
+        display: 'block',
+        width: '100%',
+        ...styles.image,
+        ...(sxBlock && sxCaption),
+      }}
+    />
+  )
+
   return (
     <Stack as="figure" sx={{ ...styles.block, ...(sxBlock || sx) }} {...rest}>
-      {children}
+      {imageComponent}
       {caption && (
         <Flex
           as="figcaption"
@@ -26,6 +41,10 @@ const ImageBlock = ({ image, caption, sx, ...rest }) => {
       )}
     </Stack>
   )
+}
+
+ImageBlock.defaultProps = {
+  sx: {},
 }
 
 export default ImageBlock
