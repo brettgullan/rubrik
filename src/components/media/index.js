3ec6a8ccc91df2528e2bf6c4f116e18cdf8edba5
Brett Gullan <brettgullan@mac.com>
2019-11-08 09:44:32 +1100

    feat: Add ResponsiveImage, ResponsivePicture components to media exports

--- src/components/media/index.js
+++ src/components/media/index.js
@@ -1,5 +1,8 @@
 export { default as Image } from './Image'
 export { default as Picture } from './Picture'
+export { default as ResponsiveImage } from './ResponsiveImage'
+export { default as ResponsivePicture } from './ResponsivePicture'
+export { default as LazyLoad } from './LazyLoad'
+
 export { default as CloudinaryImage } from './CloudinaryImage'
 export { default as CloudinaryPicture } from './CloudinaryPicture'
-export { default as LazyLoad } from './LazyLoad'
