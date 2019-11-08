2f36e8a998708556947872a57a59f78a0c40c595
Brett Gullan <brettgullan@mac.com>
2019-11-08 11:39:00 +1100

    feat: Add convertToRebass utility function
    This just copies the main Rebass component function and parameterizes it, so it will accept a Component, rather than just assuming a `div`.

---
+++ src/utils/convert-to-rebass.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import {
+  compose,
+  space,
+  layout,
+  typography,
+  color,
+  flexbox,
+} from 'styled-system'
+import css, { get } from '@styled-system/css'
+import shouldForwardProp from '@styled-system/should-forward-prop'
+
+// ----------------------------------------------------------------------------
+
+const sx = (props) => css(props.sx)(props.theme)
+const base = (props) => css(props.__css)(props.theme)
+const variant = ({ theme, variant, tx = 'variants' }) =>
+  css(get(theme, tx + '.' + variant, get(theme, variant)))(theme)
+
+// ----------------------------------------------------------------------------
+
+export const convertToRebass = (Component) =>
+  styled(Component, {
+    shouldForwardProp,
+  })(
+    {
+      boxSizing: 'border-box',
+    },
+    base,
+    variant,
+    sx,
+    (props) => props.css,
+    compose(
+      space,
+      layout,
+      typography,
+      color,
+      flexbox
+    )
+  )
+
+export default convertToRebass
