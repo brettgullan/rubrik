b8ee7ce35f4d934826e2f5c4ac383ffba1ebbbb1
Brett Gullan <brettgullan@mac.com>
2019-11-13 17:26:42 +1100

    feat: Export Tab components from structure index

--- src/components/structure/index.js
+++ src/components/structure/index.js
@@ -11,3 +11,5 @@ export { default as FauxBlockLink } from './FauxBlockLink'
 
 export { Tile, TileLayer } from './Tile'
 export { default as Scrim } from './Scrim'
+
+export { Tabs, TabList, Tab, TabPanel } from './Tabs'
