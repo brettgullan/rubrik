91299357219fdd0b1a870e4723dee2a19a706d89
Brett Gullan <brettgullan@mac.com>
2019-11-13 13:52:09 +1100

    feat: Add support for vertical Tabs

--- src/components/structure/Tabs/index.js
+++ src/components/structure/Tabs/index.js
@@ -5,6 +5,7 @@ import React, {
   forwardRef,
   useContext,
 } from 'react'
+import PropTypes from 'prop-types'
 import cx from 'classnames'
 
 import { useTabs } from '@zendeskgarden/container-tabs'
@@ -14,60 +15,137 @@ import { Box, Flex } from 'rebass'
 
 const TabsState = createContext()
 
-export const Tabs = ({ options, children, ...rest }) => {
+export const useTabState = () => useContext(TabsState)
+
+// ----------------------------------------------------------------------------
+
+export const Tabs = ({
+  direction,
+  selectedIndex,
+  options,
+  children,
+  sx,
+  ...rest
+}) => {
+  options.direction = direction
+  options.defaultSelectedIndex = selectedIndex
+
   const state = useTabs(options)
   const { getTabPanelProps } = state
+
+  // pre-process children
   let panelIndex = 0
+  const _children = children.map((child) => {
+    if (child.type.displayName === 'TabPanel') {
+      const tabPanelProps = getTabPanelProps({
+        index: panelIndex,
+        item: panelIndex,
+        key: panelIndex,
+      })
+      panelIndex++
+      return cloneElement(child, tabPanelProps)
+    } else if (child.type.displayName === 'TabList') {
+      return cloneElement(child, { direction })
+    }
+    return child
+  })
+
+  // determine structural styles (directional)
+  const styles = {
+    // only apply for vertical tabs ...
+    ...(direction === 'vertical' && {
+      display: 'flex',
+    }),
+  }
 
   return (
     <TabsState.Provider value={state}>
-      <Box {...rest}>
-        {children.map((child) => {
-          if (child.type.displayName === 'TabPanel') {
-            const tabPanelProps = getTabPanelProps({
-              index: panelIndex,
-              item: panelIndex,
-              key: panelIndex,
-            })
-            panelIndex++
-            return cloneElement(child, tabPanelProps)
-          }
-          return child
-        })}
+      <Box sx={{ ...styles, ...sx }} {...rest}>
+        {_children}
       </Box>
     </TabsState.Provider>
   )
 }
 
-export const useTabState = () => useContext(TabsState)
+Tabs.defaultProps = {
+  options: {
+    direction: 'horizontal',
+    idPrefix: 'tabs',
+    defaultSelectedIndex: 0,
+  },
+  direction: 'horizontal',
+  selectedIndex: 0,
+}
 
-export const TabList = ({ children, ...rest }) => {
+// ----------------------------------------------------------------------------
+
+export const TabList = ({ direction, children, sx, ...rest }) => {
   const { getTabListProps, getTabProps, selectedItem } = useTabState()
+
+  // pre-process tabs
+  const tabs = children.map((child, index) => {
+    const ref = createRef(null)
+    const props = getTabProps({
+      index,
+      item: index,
+      key: index,
+      ref: ref,
+      focusRef: ref,
+    })
+    props.className = cx(child.props.className, {
+      active: index == selectedItem,
+    })
+    return cloneElement(child, props)
+  })
+
+  // determine structural styles (directional)
+  const styles = {
+    m: 0,
+    p: 0,
+    listStyle: 'none',
+    flexShrink: 0,
+
+    // only apply for vertical tabs ...
+    ...(direction === 'vertical' && {
+      flexDirection: 'column',
+    }),
+  }
+
   return (
-    <Flex as="ul" {...getTabListProps()} {...rest}>
-      {children.map((child, index) => {
-        const ref = createRef(null)
-        const props = getTabProps({
-          index,
-          item: index,
-          key: index,
-          ref: ref,
-          focusRef: ref,
-        })
-        props.className = cx(child.props.className, {
-          active: index == selectedItem,
-        })
-        return cloneElement(child, props)
-      })}
+    <Flex
+      as="ul"
+      sx={{
+        ...styles,
+        ...sx,
+      }}
+      {...getTabListProps()}
+      {...rest}
+    >
+      {tabs}
     </Flex>
   )
 }
 
-export const Tab = forwardRef((props, ref) => (
-  <Box as="li" ref={ref} {...props} />
+// ----------------------------------------------------------------------------
+
+export const Tab = forwardRef(({ sx, ...rest }, ref) => (
+  <Box
+    as="li"
+    sx={{
+      // minWidth: 'auto',
+      // whiteSpace: 'nowrap',
+      ...sx,
+    }}
+    ref={ref}
+    {...rest}
+  />
 ))
 
+// ----------------------------------------------------------------------------
+
 export const TabPanel = (props) => <Box {...props} />
+
+// ----------------------------------------------------------------------------
 
 /*
 export const Tabs = ({ options, children, ...rest }) => {
--- src/components/structure/Tabs/tabs.stories.js
+++ src/components/structure/Tabs/tabs.stories.js
@@ -19,13 +19,120 @@ export default {
 
 export const Primary = () => {
   return (
-    <Tabs sx={{ p: 4 }}>
+    <Tabs selectedIndex={1} sx={{ p: 4 }}>
       <TabList
         sx={{
-          m: 0,
-          p: 0,
           mb: 4,
-          listStyle: 'none',
+        }}
+      >
+        <Tab>Tab 1</Tab>
+        <Tab>Tab 2</Tab>
+        <Tab>Tab 3</Tab>
+      </TabList>
+
+      <TabPanel>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </TabPanel>
+      <TabPanel>
+        Tempor id eu nisl nunc. Amet cursus sit amet dictum sit. Gravida arcu ac
+        tortor dignissim convallis aenean. Lectus arcu bibendum at varius vel
+        pharetra. Commodo odio aenean sed adipiscing diam donec adipiscing. Amet
+        consectetur adipiscing elit duis tristique sollicitudin nibh sit amet.
+        Adipiscing elit pellentesque habitant morbi tristique senectus. Tempor
+        nec feugiat nisl pretium fusce id. Diam sollicitudin tempor id eu nisl.
+        Scelerisque in dictum non consectetur a erat nam at lectus. Eget velit
+        aliquet sagittis id consectetur purus.
+      </TabPanel>
+      <TabPanel>
+        Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae. Orci a
+        scelerisque purus semper eget. Sit amet volutpat consequat mauris.
+        Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque
+        eu. Consequat ac felis donec et odio pellentesque diam volutpat commodo.
+        Consequat nisl vel pretium lectus quam id leo in. Malesuada fames ac
+        turpis egestas maecenas pharetra convallis. Et egestas quis ipsum
+        suspendisse. Dictum non consectetur a erat nam at lectus. Etiam tempor
+        orci eu lobortis elementum nibh tellus molestie nunc. Nisl condimentum
+        id venenatis a condimentum vitae sapien. Dictum sit amet justo donec
+        enim diam vulputate. Interdum velit laoreet id donec ultrices tincidunt
+        arcu non sodales.
+      </TabPanel>
+    </Tabs>
+  )
+}
+
+// ----------------------------------------------------------------------------
+
+export const Vertical = () => {
+  return (
+    <Tabs direction="vertical" sx={{ p: 4 }}>
+      <TabList
+        sx={{
+          mb: 4,
+          mr: 4,
+        }}
+      >
+        <Tab>Tab 1</Tab>
+        <Tab>Tab 2</Tab>
+        <Tab>Tab 3</Tab>
+      </TabList>
+
+      <TabPanel>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </TabPanel>
+      <TabPanel>
+        Tempor id eu nisl nunc. Amet cursus sit amet dictum sit. Gravida arcu ac
+        tortor dignissim convallis aenean. Lectus arcu bibendum at varius vel
+        pharetra. Commodo odio aenean sed adipiscing diam donec adipiscing. Amet
+        consectetur adipiscing elit duis tristique sollicitudin nibh sit amet.
+        Adipiscing elit pellentesque habitant morbi tristique senectus. Tempor
+        nec feugiat nisl pretium fusce id. Diam sollicitudin tempor id eu nisl.
+        Scelerisque in dictum non consectetur a erat nam at lectus. Eget velit
+        aliquet sagittis id consectetur purus.
+      </TabPanel>
+      <TabPanel>
+        Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae. Orci a
+        scelerisque purus semper eget. Sit amet volutpat consequat mauris.
+        Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque
+        eu. Consequat ac felis donec et odio pellentesque diam volutpat commodo.
+        Consequat nisl vel pretium lectus quam id leo in. Malesuada fames ac
+        turpis egestas maecenas pharetra convallis. Et egestas quis ipsum
+        suspendisse. Dictum non consectetur a erat nam at lectus. Etiam tempor
+        orci eu lobortis elementum nibh tellus molestie nunc. Nisl condimentum
+        id venenatis a condimentum vitae sapien. Dictum sit amet justo donec
+        enim diam vulputate. Interdum velit laoreet id donec ultrices tincidunt
+        arcu non sodales.
+      </TabPanel>
+    </Tabs>
+  )
+}
+
+// ----------------------------------------------------------------------------
+
+export const VerticalTabsOnRight = () => {
+  return (
+    <Tabs
+      direction="vertical"
+      sx={{
+        p: 4,
+        flexDirection: 'row-reverse',
+      }}
+    >
+      <TabList
+        sx={{
+          mb: 4,
+          mr: 4,
         }}
       >
         <Tab>Tab 1</Tab>
