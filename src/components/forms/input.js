6e92f80b08a5d3dcfceb430636a8588658e3b43f
Brett Gullan <brettgullan@mac.com>
2019-11-14 08:14:15 +1100

    improve: Convert Input and Error components to use useVariant hook

--- src/components/forms/input.js
+++ src/components/forms/input.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import cx from 'classnames'
 
-import { useTheme } from 'emotion-theming'
-import { get } from '@styled-system/css'
-
 import { useField } from 'formik'
 import { Input as InputField } from '@rebass/forms'
+
+import { useVariant } from '../../hooks'
 
 // ----------------------------------------------------------------------------
 
 const Input = ({ name, size, className, sx, ...rest }) => {
   const [field, meta] = useField(name)
-  const theme = useTheme()
 
   const { touched, error } = meta
   const states = { error: touched && error, valid: touched && !error }
 
-  const sizingStyles = get(theme, `forms.input.${size}`, null)
+  const sizingStyles = useVariant(`forms.input.${size}`)
 
   return (
     <InputField
