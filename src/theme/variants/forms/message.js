8893a66c77c60c4df70b4aa01c21700622dfa617
Brett Gullan <brettgullan@mac.com>
2019-11-14 08:15:15 +1100

    improve: Refactor Forms theme variants

---
+++ src/theme/variants/forms/message.js
@@ -0,0 +1,29 @@
+import fontSizes from '../../fontsizes'
+
+// ----------------------------------------------------------------------------
+
+/*
+ *  FORM MESSAGE STYLES
+ */
+const message = {
+  sm: {
+    fontSize: fontSizes.minion,
+  },
+  md: {
+    fontSize: fontSizes.brevier,
+  },
+  lg: {
+    fontSize: fontSizes.longPrimer,
+  },
+
+  error: {
+    fontFamily: 'text',
+    fontWeight: 'regular',
+    lineHeight: 'snug',
+    color: 'form.error',
+  },
+}
+
+// ----------------------------------------------------------------------------
+
+export default message
