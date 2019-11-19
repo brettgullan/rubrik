4875c67791f734d832efb2ee07a8fd9baae873a5
Brett Gullan <brettgullan@mac.com>
2019-11-19 15:34:46 +1100

    feat: Update @rubrik/media bundling

---
+++ packages/media/src/providers/index.js
@@ -0,0 +1,4 @@
+export { default as ResponsiveProvider } from './Responsive'
+export { ResponsiveContext, useResponsiveBuilder } from './Responsive'
+
+export { default as CloudinaryAdaptor } from './Responsive/cloudinary'
