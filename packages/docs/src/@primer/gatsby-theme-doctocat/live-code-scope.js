63fa7d9b2fb41cda04c18fe616522245f1fc2f67
Brett Gullan <brettgullan@mac.com>
2019-11-20 09:46:41 +1100

    fix: Resolve linting error in Doctotat docs
    Seems there may be a mismatch between current Gatsby version and ESLint.
    Following Gatsby’s documentation to customize/override eslint settings.

---
+++ packages/docs/src/@primer/gatsby-theme-doctocat/live-code-scope.js
@@ -0,0 +1,10 @@
+import { Alpha, Beta, Gamma, Delta } from '@rubrik/core'
+
+// ----------------------------------------------------------------------------
+
+export default {
+  Alpha,
+  Beta,
+  Gamma,
+  Delta,
+}
