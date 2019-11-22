57e389556d55a0a5a1d8d1e0837580fb684c47ab
Brett Gullan <brettgullan@mac.com>
2019-11-22 16:47:07 +1100

    fix: Customize Docz theme font

--- packages/docz/doczrc.js
+++ packages/docz/doczrc.js
@@ -1,6 +1,10 @@
 export default {
   menu: ['Overview', 'Components'],
   themeConfig: {
+    fonts: {
+      body: "'Barlow', 'sans-serif'",
+      heading: "'Barlow', 'sans-serif'",
+    },
     colors: {
       header: {
         bg: 'tomato',
