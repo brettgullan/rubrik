7de063e2776d0c757d3535910f215225301401e1
Brett Gullan <brettgullan@mac.com>
2019-11-22 16:28:28 +1100

    feat: Add Google fonts support
    I hope!

--- packages/docz/gatsby-config.js
+++ packages/docz/gatsby-config.js
@@ -5,5 +5,16 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Barlow`,
+            variants: ['100','100i','200','200i','300','300i','400','400i','500','500i','600','600i','700','700i','800','800i','900','900i']
+          },
+        ],
+      },
+    }
   ],
 }
