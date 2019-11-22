0d8e8f56e46e90ab8210853424cd569daae43b28
Brett Gullan <brettgullan@mac.com>
2019-11-22 16:46:47 +1100

    style: comments

--- packages/docz/src/components/ThemeWrapper.js
+++ packages/docz/src/components/ThemeWrapper.js
@@ -5,6 +5,9 @@ import theme from '@rubrik/theme-default'
 
 // ----------------------------------------------------------------------------
 
+/**
+ * Display a Component under documentation, using its own theme (rather than the Docz theme).
+ */
 const ThemeWrapper = (props) => <ThemeProvider theme={theme} {...props} />
 
 export default ThemeWrapper
