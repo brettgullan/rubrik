1e154186130be3f910bc55ba4da19284b5a6b473
Brett Gullan <brettgullan@mac.com>
2019-11-25 13:57:46 +1100

    fix: Export Grid component from structure

--- packages/core/src/components/structure/index.js
+++ packages/core/src/components/structure/index.js
@@ -3,6 +3,8 @@ export { default as Blanket } from './Blanket'
 export { default as FullBleed } from './FullBleed'
 export { default as FullWidth } from './FullWidth'
 
+export { default as Grid } from './Grid'
+
 export { default as Split } from './Split'
 export { default as Stack } from './Stack'
 export { default as Stretch } from './Stretch'
