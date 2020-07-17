0e7c65bf5f1545c790766ba85020a34d23fb4a01
Brett Gullan <brettgullan@mac.com>
2020-07-17 16:33:02 +1000

    feat: Basic restore of original Docz pages
    - Documentation is incomplete, but consistent with what was there before
    - Need to restore ThemeWrapper capability
    - Ideally restore/provide some level of formatted code examples (this is apparently provided by Docusaurus, need to explore).

--- packages/docs/sidebars.js
+++ packages/docs/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Components: ['grid'],
-    Features: ['mdx'],
+    Media: ['media', 'responsive-image', 'responsive-picture'],
+    Structure: ['full-bleed', 'full-width', 'grid', 'stack', 'split', 'stretch', 'tile'],
+    Typography: ['typography'],
   },
 };
