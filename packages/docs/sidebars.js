84536cc53120db9bee75768d3586a7a1c6928b0c
Brett Gullan <brettgullan@mac.com>
2020-07-30 09:59:54 +1000

    feat: Configure introductory pages into navigation

--- packages/docs/sidebars.js
+++ packages/docs/sidebars.js
@@ -1,5 +1,6 @@
 module.exports = {
   someSidebar: {
+    Introduction: ['about', 'getting-started'],
     Media: ['media', 'responsive-image', 'responsive-picture'],
     Structure: ['full-bleed', 'full-width', 'grid', 'stack', 'split', 'stretch', 'tile'],
     Typography: ['typography'],
