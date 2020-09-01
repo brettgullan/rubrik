59d55e0dc9fa615e8095f807ab9ebe6f1b4ae551
Brett Gullan <brettgullan@mac.com>
2020-09-02 09:58:05 +1000

    wip: Documentation

--- packages/docs/sidebars.js
+++ packages/docs/sidebars.js
@@ -1,6 +1,12 @@
 module.exports = {
   someSidebar: {
     Introduction: ['about', 'getting-started'],
+    Core: [{
+      Media: [ 
+        'core/image',
+        'core/picture',
+      ],
+    }],
     Media: ['media', 'responsive-image', 'responsive-picture'],
     Structure: ['full-bleed', 'full-width', 'grid', 'stack', 'split', 'stretch', 'tile'],
     Typography: ['typography'],
